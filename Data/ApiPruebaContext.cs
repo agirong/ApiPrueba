6d5afb0d6044509839a5a0e730f700de5311cb0b
agirong <62667714+agirong@users.noreply.github.com>
2021-09-13 06:30:26 -0600

    Agregar archivos de proyecto.

---
+++ Data/ApiPruebaContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ApiPrueba.Modelo;
+
+namespace ApiPrueba.Data
+{
+    public class ApiPruebaContext : DbContext
+    {
+        public ApiPruebaContext (DbContextOptions<ApiPruebaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ApiPrueba.Modelo.Producto> Producto { get; set; }
+    }
+}
---
+++ Modelo/Producto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiPrueba.Modelo
+{
+    public class Producto
+    {
+        public int id { get; set; }
+
+        public int Nombre { get; set; }
+
+        public int PrecioCompra { get; set; }
+
+        public int PrecioVenta { get; set; }
+
+        public int Stock { get; set; }
+
+    }
+}
